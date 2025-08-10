cd4f23e2002ea00922ee06ca533752fb2432e0b9
AdamDono <flipbrickzmusic1@gmail.com>
2025-08-10 02:23:58 +0200

    everything works

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,11 @@ from .config import Config
 
 db = SQLAlchemy()
 
+
+
+
+
+
 def create_app():
     app = Flask(__name__, template_folder='../../in-needfrontend/templates', static_folder='../../in-needfrontend/static')
     app.config.from_object(Config)
